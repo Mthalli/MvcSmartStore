53c728a0ad2766e766f4efde220acd5eccc99d3f
Mateusz <mkozicki@outlook.com>
2025-05-16 14:11:08 +0200

    Categories added

--- MvcSmartStore/Controllers/SmartphoneController.cs
+++ MvcSmartStore/Controllers/SmartphoneController.cs
@@ -11,15 +11,29 @@ namespace MvcSmartStore.Controllers
         {
             _db = db;
         }
-        public IActionResult Index()
+        public IActionResult All(string brand, float? minPrice, float? maxPrice, int? ram, int? refresh_rate, int? storage)//?Nullable
         {
-            return View();
+            var query = _db.Smartphones.AsQueryable();
+
+            if (!string.IsNullOrEmpty(brand)) query = query.Where(r => r.Brand == brand);
+            if (minPrice.HasValue) query = query.Where(r => r.Price >= minPrice.Value);                
+            if (maxPrice.HasValue) query = query.Where(r => r.Price <= maxPrice.Value);                
+            if (ram.HasValue) query = query.Where(r => r.Ram == ram.Value);
+            //dodane
+            if(refresh_rate.HasValue) query = query.Where(r => r.Refresh_rate ==  refresh_rate.Value);
+            if(storage.HasValue) query = query.Where(r => r.Storage == storage.Value);
+
+            // Dropdown Data
+            ViewBag.Brands = _db.Smartphones.Select(r => r.Brand).Distinct().ToList();
+            ViewBag.Rams = _db.Smartphones.Select(r => r.Ram).Distinct().OrderBy(r => r).ToList();
+            ViewBag.Refresh_rate = _db.Smartphones.Select(r => r.Refresh_rate).Distinct().OrderBy(r => r).ToList();
+            ViewBag.Storage = _db.Smartphones.Select(r => r.Storage).Distinct().OrderBy(r=>r).ToList();
+
+            var result = query.ToList();
+
+            return View(result);
         }
-        public IActionResult All()
-        {
-            var smartphones = _db.Smartphones.ToList();
-            return View(smartphones);
-        }
+
     }
 
 }
