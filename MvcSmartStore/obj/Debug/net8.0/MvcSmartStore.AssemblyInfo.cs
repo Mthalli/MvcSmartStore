bb21eb0ed37363f3333d9c79e3848ff336637e4e
Mateusz <mkozicki@outlook.com>
2025-05-16 09:51:11 +0200

    Changed view

--- MvcSmartStore/obj/Debug/net8.0/MvcSmartStore.AssemblyInfo.cs
+++ MvcSmartStore/obj/Debug/net8.0/MvcSmartStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcSmartStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+995bfa4b13c80b0bf22a54a7519344e69cf90cf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcSmartStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcSmartStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
